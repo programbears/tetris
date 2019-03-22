4a9124f69d0841e9a0b82cde7b41120a089e7fc5
Willem Garnier <willem.garnier@gmail.com>
2019-03-22 00:32:26 +0000

    fix serveStatic logging

--- server/lib/utils.js
+++ server/lib/utils.js
@@ -5,10 +5,10 @@ function serveStatic(router, directory, { recursive = true, logger, root } = {})
     fs.readdirSync(directory, { withFileTypes: true }).forEach((file) => {
         const filepath = path.join(directory, file.name);
         const url = `/${root ? path.join(root, file.name) : file.name}`;
-        logger(`serving ${url} as static ${filepath}`);
         if (file.isFile()) {
+            logger(`serving ${url} as static ${filepath}`);
             router.get(url, function(req, res) {
-                if (logger) logger(`GET ${file.name} static`);
+                if (logger) logger(`GET ${req.originalUrl} static ${file.name}`);
                 res.sendFile(filepath);
             });
         } else if (file.isDirectory() && recursive) {
